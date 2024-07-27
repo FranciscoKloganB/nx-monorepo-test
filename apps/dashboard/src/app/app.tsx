400412120bf26912ba18a6cfdee66ebc4e350bdb
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2024-07-27 11:47:14 +0100

    refactor(dashboard): add bigger and slower animation

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -8,12 +8,14 @@ export function App() {
   return (
     <VStack minH="100vh" justifyContent="center">
       <Box
-        h="60px"
-        w="60px"
+        animationDuration="2000ms"
+        animationName="fade-in"
+        animationTimingFunction="ease-in"
+        backgroundColor="nice-yellow"
         borderRadius="50%"
-        bg="nice-yellow"
-        animation="bounce"
-      ></Box>
+        height="44"
+        width="44"
+      />
       <Box className={css({ bg: 'red.300' })}>
         <Button>Primary</Button>
       </Box>
